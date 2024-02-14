1f3e3da33280d58c7fbb402652ab3e867c315dec
Snay77 <77ethan.barlet@gmail.com>
2024-02-14 09:25:29 +0100

    starting the form

---
+++ Client/src/Form/form.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+function AddArticleForm(){
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    return(
+        <>
+        Salut
+        </>
+    )
+}
