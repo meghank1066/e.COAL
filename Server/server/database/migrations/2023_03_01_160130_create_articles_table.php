43790b5b2baac17c38791a3d0a41bfacfa667763
Killian <killianm2259@gmail.com>
2024-02-14 19:17:30 +0100

    update + delete

--- Server/server/database/migrations/2023_03_01_160130_create_articles_table.php
+++ Server/server/database/migrations/2023_03_01_160130_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('lead');
+            $table->string('lead')->nullable();
             $table->text('content');
             $table->string('thumbnailURL');
             $table->string('mediaType')->nullable();
