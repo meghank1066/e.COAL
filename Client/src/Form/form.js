f14eb3fcb66d579edfaf171af715bf5f0a626893
Meghank1066 <D00254682@student.dkit.ie>
2024-02-15 22:08:40 +0000

    form

--- Client/src/Form/form.js
+++ Client/src/Form/form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import styles from './form.module.css'
 
 function AddArticleForm() {
     const [formData, setFormData] = useState({ title: "", content: "", tag: "", thumbnailURL: "", mediaURL: "" });
@@ -7,6 +8,7 @@ function AddArticleForm() {
     const config = {
         headers : {"Authorization" : `${localStorage.getItem("token_type")} ${localStorage.getItem("token")}`}
     }
+    
 
     function handlechange(e) {
         setFormData({ ...formData, [e.target.name]: e.target.value })
@@ -24,31 +26,46 @@ function AddArticleForm() {
 
     }
     return (
-        <form onSubmit={handlesubmit}>
-            <label>
-                Title :
-                <input type="text" name="title" onChange={handlechange}></input>
+        <form onSubmit={handlesubmit} className={styles.form}>
+            <label className={styles.label}>
+                <h1>New Article</h1>
             </label>
-            <label>
-                Content :
-                <input type="text" name="content" onChange={handlechange}></input>
+            <label className={styles.label}>
+                <input type="text" name="title" placeholder="Add Title" value={formData.title} onChange={handlechange}></input>
             </label>
-            <label>
-                Tag :
-                <input type="text" name="tags" onChange={handlechange}></input>
+
+            <label className={styles.label}>
+    <textarea
+        style={{
+            width: "380px",
+            height: "150px",
+            maxWidth: "380px",
+            minWidth: "380px",
+            minHeight: "100px",
+            maxHeight: "200px",
+            border: "0.5px solid black",
+            borderRadius: "5px 5px 5px 5px"
+        }}
+        className={`${styles.commentBox} ${styles.label} ${styles.draggable}`}
+        name="content"
+        value={formData.content}
+        placeholder="Add Content.."
+        onChange={handlechange}
+    />
+    
+</label>
+            <label className={styles.label}>
+                <input type="text" name="tags" placeholder="#" onChange={handlechange}></input>
             </label>
-            <label>
-                ThumbnailURL :
-                <input type="text" name="thumbnailURL" onChange={handlechange}></input>
+            <label className={styles.label}>
+                <input type="text" name="thumbnailURL" placeholder="Enter Thumbnail" value={formData.thumbnailURL} onChange={handlechange}></input>
             </label>
-            <label>
-                mediaURL :
-                <input type="text" name="mediaURL" onChange={handlechange}></input>
+            <label className={styles.label}>
+                <input type="text" name="mediaURL" placeholder="Enter Image URL" value={formData.mediaURL} onChange={handlechange}></input>
             </label>
-            <label>
-                Submit :
-                <input type="submit"></input>
-            </label>
+            <label className={styles.label}>
+        <input type="submit" className={styles.button} onClick={""} />
+    </label>
         </form>
     )
 }
