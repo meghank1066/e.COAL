c4e439867ae0233fff7dd084380b855407b1a421
Snay77 <77ethan.barlet@gmail.com>
2024-02-15 22:11:55 +0100

    adding the articles tag on the home

---
+++ Client/src/ArticleTag/articletag.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Article from "../Article/article";
+
+function ArticleTag(props){
+    console.log(props.articles)
+    const filteredArticles = props.articles.filter(  (article) => article.tags.some( x => x.name == props.tag))
+    return(
+        <div>
+            <h2>{props.tag} :</h2>
+            {filteredArticles.map( x => <Article title={x.title} thumbnailURL={x.thumbnailURL} id={x.id}/>)}
+        </div>
+    )
+}
+
+export default ArticleTag
--- Client/src/Home/home.js
+++ Client/src/Home/home.js
@@ -1,10 +1,30 @@
 import styles from './home.module.css'
+import React from "react";
+import ArticleTag from '../ArticleTag/articletag';
+import { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function Home(){
-        //
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        async function fetchArticles() {
+            try {
+                const response = await axios.get("http://127.0.0.1:8000/api/articles")
+                setArticles(response.data)
+            } catch (error) {
+                console.error("Error fetching articles", error)
+            }
+            console.log(articles.articles)
+        }
+
+        fetchArticles()
+    }, [])
     return(
         <section>
-            Welcome to YumYum Home
+            <ArticleTag tag="ecoal23" articles={articles} />
+            <ArticleTag tag="avion" articles={articles} />
         </section>
     )
 }
