b7d0a911c996ffe4a37ea8a2471cf111a4b5452f
Lou-Anne <saejinmx@gmail.com>
2024-02-15 22:27:20 +0100

    Values

--- Client/src/Home/home.js
+++ Client/src/Home/home.js
@@ -3,13 +3,31 @@ import { HeaderL } from '../HeaderL/headerL'
 
 
 
-function Home(){
-        //
-    return(
-        <section>
-            <HeaderL/>
+function Home() {
 
-            
+    return (
+        <section>
+            <HeaderL />
+
+            <div className={styles.values}>
+                <h1>
+                    Our Values
+                </h1>
+                <div>
+                    <div>
+                        <box-icon type='solid' name='heart'></box-icon>
+                        <p>Healthy</p>
+                    </div>
+                    <div>
+                    <box-icon name='dish' type='solid' ></box-icon>
+                    <p>Tasty</p>
+                    </div>
+                    <div>
+                    <box-icon name='euro' ></box-icon>
+                    <p>Cheap</p>
+                    </div>
+                </div>
+            </div>
 
         </section>
     )
