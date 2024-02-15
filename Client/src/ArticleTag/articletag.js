4ce0e4fc774dd562e344d32ee6c50a9b52492618
Lou-Anne <saejinmx@gmail.com>
2024-02-15 23:39:48 +0100

    Home finish

--- Client/src/ArticleTag/articletag.js
+++ Client/src/ArticleTag/articletag.js
@@ -6,7 +6,7 @@ function ArticleTag(props){
     const filteredArticles = props.articles.filter(  (article) => article.tags.some( x => x.name == props.tag))
     return(
         <div>
-            <h2>{props.tag} :</h2>
+            {/* <h2>{props.tag} :</h2> */}
             {filteredArticles.map( x => <Article title={x.title} thumbnailURL={x.thumbnailURL} id={x.id}/>)}
         </div>
     )
