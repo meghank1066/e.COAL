630e677a530bd963f8e16a1a5c8f99930d0c34a7
Snay77 <77ethan.barlet@gmail.com>
2024-02-15 16:08:40 +0100

    add x bouton

--- Client/src/ArticleAccount/articleaccount.js
+++ Client/src/ArticleAccount/articleaccount.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Article from "../Article/article";
+import ArticleDelete from "../ArticleDelete/articledelete";
 
 function ArticleAccount() {
     const [articles, setArticles] = useState({})
@@ -28,7 +28,7 @@ function ArticleAccount() {
             <p>{articles.name}</p>
             <p>test</p>
             <h2>Your Articles</h2>
-            {articles.articles?.map(x => <Article title={x.title} thumbnailURL={x.thumbnailURL} id={x.id} />)}
+            {articles.articles?.map(x => <ArticleDelete title={x.title} thumbnailURL={x.thumbnailURL} id={x.id} setArticles={setArticles}/>)}
         </div>
     )
 }
