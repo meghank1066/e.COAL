98167babe85b61f348e2d489d46502f5cc28d827
Lou-Anne <saejinmx@gmail.com>
2024-02-16 02:33:45 +0100

    Update OK

--- Client/src/Update/update.js
+++ Client/src/Update/update.js
@@ -1,32 +1,33 @@
-import styles from './update.module.css';
+import styles from '../Form/form.module.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
+import { HeaderT } from "../HeaderT/headerT"
 
 function Update() {
-    
+
     let params = useParams()
     const [article, setArticle] = useState({})
-    
+
     async function getArticle() {  // The function is asynchronous
         const data = (await axios.get(`http://localhost:8000/api/articles/${params.id}`)).data
         setArticle(data)
     }
-    
+
     useEffect(() => { // this is a hook called everytime the function is rendered again
         // Don't forget to import useEffect
         getArticle()
     }, []);
-    
+
     const taglist = article.tags?.map(tag => tag.name).join(' ');
 
 
 
-    
-    const [formData, setFormData] = useState({title: article.title, content: article.content, tags : taglist, thumbnailURL: article.thumbnailURL, mediaURL: article.mediaURL});
+
+    const [formData, setFormData] = useState({ title: article.title, content: article.content, tags: taglist, thumbnailURL: article.thumbnailURL, mediaURL: article.mediaURL });
 
     const config = {
-        headers : {"Authorization" : `${localStorage.getItem("token_type")} ${localStorage.getItem("token")}`}
+        headers: { "Authorization": `${localStorage.getItem("token_type")} ${localStorage.getItem("token")}` }
     }
 
     function handlechange(e) {
@@ -43,36 +44,23 @@ function Update() {
             console.error("Error updating article:", error);
             console.log(formData)
         }
-        
+
 
     }
     return (
-        <form onSubmit={handlesubmit}>
-            <label>
-                Title :
-                <input type="text" defaultValue={article.title ? article.title : ''} name="title" onChange={handlechange}></input>
-            </label>
-            <label>
-                Content :
-                <input type="text" defaultValue={article.content ? article.content : ''} name="content" onChange={handlechange}></input>
-            </label>
-            <label>
-                Tag :
-                <input type="text" defaultValue={article.tags ? taglist : ''} name="tags" onChange={handlechange}></input>
-            </label>
-            <label>
-                ThumbnailURL :
-                <input type="text" defaultValue={article.thumbnailURL ? article.thumbnailURL : ''} name="thumbnailURL" onChange={handlechange}></input>
-            </label>
-            <label>
-                mediaURL :
-                <input type="text" defaultValue={article.mediaURL ? article.mediaURL : ''} name="mediaURL" onChange={handlechange}></input>
-            </label>
-            <label>
-                Submit :
-                <input type="submit"></input>
-            </label>
-        </form>
+        <>
+            <HeaderT />
+            <h1 id={styles.title}>Modify your article</h1>
+            <form onSubmit={handlesubmit} className={styles.form}>
+                <input type="text" placeholder="Title" defaultValue={article.title ? article.title : ''} name="title" onChange={handlechange}></input>
+                <input type="text" placeholder="Tags (seperater by a space)" defaultValue={article.tags ? taglist : ''} name="tags" onChange={handlechange}></input>
+                <input type="text" placeholder="Link of the image in preview" defaultValue={article.thumbnailURL ? article.thumbnailURL : ''} name="thumbnailURL" onChange={handlechange}></input>
+                <input type="text" placeholder="Link of the article image" defaultValue={article.mediaURL ? article.mediaURL : ''} name="mediaURL" onChange={handlechange}></input>
+                <textarea placeholder="Write your article here ..." className={styles.special} rows="10" defaultValue={article.content ? article.content : ''} name="content" onChange={handlechange}></textarea>
+                <input type="submit" value="Send"></input>
+            </form>
+        </>
+
     )
 }
 
